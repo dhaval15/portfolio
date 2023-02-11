358be79cb7de540d5c25741eb4bf4225e11b76fc
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-02-11 09:59:42 +0000

    added about me

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -6,18 +6,12 @@ export default {
 				{
 					id: 1,
 					bio:
-						'Sou uma alma criativa e curiosa, que encontrou no design uma maneira de levar a vida exercitanto sempre as aréas que mais me interessam e estimulam. Estou sempre em busca de novas inspirações e antenada nas trends, tudo para produzir um conteúdo com mais valor e evoluir como profissional.',
+						'I am a creative and curious individul, who loves programming and technologies   that  sorrounds   it.   I   find  exploring   new technologies and  new domains, an interesting  learning process, and often myself  find trying out technologies  that are totally different from  my skillset, which  allows me to  understand the concepts better and evolve as a professional.',
 				},
 				{
 					id: 2,
 					bio:
-						'Encontrei no Design uma forma de continuar a ser curiosa, descobrindo coisas novas e de certa forma fazendo arte. Mas, principalmente, o Design me possibilita utilizar uma das minhas ‘skills’ mais poderosas, a empatia. Com o passar dos anos, a gente vai aprendendo a ser mais dura, mas ver o lado bom das pessoas e entender o que se passa nelas, é essencial não só para o lado profissional, mas principalmente humano.',
-				},
-
-								{
-					id: 3,
-					bio:
-						'Cada vez mais, sei que meu trabalho vai muito além do visual: se trata de transmitir essências, uma mensagem para ser contada junto com boas histórias!',
+						'I believe programming and  developing technologies in general as creative as  art, and sometimes find  myself exploring paradigms that  affects better  development process.  I also  love writing about technolgy, movies, books and on rare ocassions psychology.',
 				},
 			],
 		};
@@ -29,23 +23,29 @@ export default {
 	<div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
 		<!-- About profile image -->
 		<!--
-		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
+		<div class="w-full sm:w-2/3 mb-7 sm:mb-0">
+			
 			<img
 				src="@/assets/images/profile.jpeg"
 				class="rounded-xl w-96"
 				alt=""
 			/>
-		</div> -->
+		</div> 
+		-->
 
 		<!-- About details -->
-		<div class="w-full sm:w-3/4 text-left">
+		<div class="w-full sm:w-2/3 text-left">
 			<p
 				v-for="bio in bios"
 				:key="bio.id"
+				style="text-align:justify"
 				class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
 			>
 				{{ bio.bio }}
 			</p>
 		</div>
+
+		<div class="w-full sm:w-1/3 mb-7 sm:mb-0">
+		</div> 
 	</div>
 </template>
